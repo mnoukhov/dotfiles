7b04124ad21f27b44d00e021e134d13209e8fb17
Michael Noukhovitch <mnoukhov@gmail.com>
2024-01-28 20:05:02 +0000

    updated init lua to be more lua

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
 local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
 local g = vim.g      -- a table to access global variables
 local opt = vim.opt  -- to set options
@@ -18,21 +17,23 @@ require "paq" {
     {'saadparwaiz1/cmp_luasnip'};        -- Snippets source for nvim-cmp
     {'L3MON4D3/LuaSnip'};                -- Snippets plugin
     {'lervag/vimtex'};
-    {'ojroques/nvim-lspfuzzy'};
+    {'joechrisellis/lsp-format-modifications.nvim'};
 
     -- FZF
     {'junegunn/fzf'};
     {'junegunn/fzf.vim'};
+    -- {'ojroques/nvim-lspfuzzy'};
 
     -- motions and ux
     {'numToStr/Comment.nvim'};
-    {"folke/which-key.nvim", run = function() vim.o.timeout = true vim.o.timeoutlen = 300 end };
+    {"folke/which-key.nvim", build = function() vim.o.timeout = true vim.o.timeoutlen = 300 end };
     {'tpope/vim-fugitive'};
     {'tpope/vim-rhubarb'};
+    {'tpope/vim-eunuch'};
     {'ggandor/leap.nvim'};
 
     -- Neotree
-    {"nvim-neo-tree/neo-tree.nvim", branch = "v2.x"};
+    {"nvim-neo-tree/neo-tree.nvim", branch = "v3.x"};
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
 
@@ -43,15 +44,18 @@ require "paq" {
     'kyazdani42/nvim-web-devicons';     -- icons for the statusline
     {'edkolev/tmuxline.vim'};
     {'kdheepak/tabline.nvim'};
-
+    --
     {'superevilmegaco/AutoRemoteSync.nvim'};
-    {'chipsenkbeil/distant.nvim'};
+    -- {'chipsenkbeil/distant.nvim'};
 }
 
 
 -------------------- OPTIONS -------------------------------
-cmd 'colorscheme seoul256'            -- Put your favorite colorscheme here
+-- colors
+opt.termguicolors = true
+vim.cmd 'colorscheme seoul256'            -- Put your favorite colorscheme here
 g.seoul256_background = 237
+
 opt.completeopt = {'menuone', 'noinsert', 'noselect'}  -- Completion options (for deoplete)
 opt.expandtab = true                -- Use spaces instead of tabs
 opt.hidden = true                   -- Enable background buffers
@@ -99,6 +103,9 @@ map('n', '<C-d>', '<cmd>bdelete<CR>')
 -- fzf --
 map('n', '<C-p>', '<cmd>Files<CR>')
 
+-- fugitive --
+map('n', '<space>g', '<cmd>Git<CR>', {desc = "Git"})
+
 -------------------- COMMENT -------------------------------
 require('Comment').setup()
 
@@ -114,53 +121,10 @@ opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 -------------------- LSP -----------------------------------
-local lsp = require 'lspconfig'
-local lspfuzzy = require 'lspfuzzy'
+local lspconfig = require 'lspconfig'
 
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, {desc = "Open Float"})
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {desc = "Go to Prev"})
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {desc = "Go to Next"})
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, {desc = "Open Location List"})
-
-local on_attach = function(client, bufnr)
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap=true, silent=false, buffer=bufnr }
-    bufopts.desc = "Declaration"
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    bufopts.desc = "Definition"
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    bufopts.desc = "References"
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    bufopts.desc = "Rename"
-    vim.keymap.set('n', '<space>r', vim.lsp.buf.rename, bufopts)
-    bufopts.desc = "Add Workspace Folder"
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    bufopts.desc = "Remove Workspace Folder"
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    bufopts.desc = "List Workspace Folders"
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    bufopts.desc = "Format"
-    vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-    bufopts.desc = "Code Action"
-    vim.keymap.set({'n','v'}, '<space>c', vim.lsp.buf.code_action, bufopts)
-    bufopts.desc = "Hover"
-    vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, bufopts)
-    bufopts.desc = "Signature Help"
-    vim.keymap.set({'n','i'}, '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.lsp.set_log_level("debug")
-
-    cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format() ]]
-end
-
-
--- We use the default settings for ccls and pylsp: the option table can stay empty
-lsp.pylsp.setup {
-    on_attach = on_attach,
+lspconfig.pylsp.setup {
+    -- on_attach = on_attach,
     filetypes = {"python"},
     settings = {
         pyslp = {
@@ -179,7 +143,73 @@ lsp.pylsp.setup {
         },
     }
 }
-lspfuzzy.setup {}  -- Make the LSP client use FZF instead of the quickfix list
+
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, {desc = "Open Float"})
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {desc = "Go to Prev"})
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {desc = "Go to Next"})
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, {desc = "Open Location List"})
+
+-- local on_attach = function(client, bufnr)
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+    -- local bufopts = { noremap=true, silent=false, buffer=bufnr }
+        local bufopts = { buffer = ev.buf }
+        bufopts.desc = "Declaration"
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        bufopts.desc = "Definition"
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        bufopts.desc = "References"
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+        bufopts.desc = "Rename"
+        vim.keymap.set('n', '<space>r', vim.lsp.buf.rename, bufopts)
+        bufopts.desc = "Add Workspace Folder"
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+        bufopts.desc = "Remove Workspace Folder"
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+        bufopts.desc = "List Workspace Folders"
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, bufopts)
+        bufopts.desc = "Format"
+        vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+        bufopts.desc = "Code Action"
+        vim.keymap.set({'n','v'}, '<space>c', vim.lsp.buf.code_action, bufopts)
+        bufopts.desc = "Hover"
+        vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, bufopts)
+        bufopts.desc = "Signature Help"
+        vim.keymap.set({'n','i'}, '<C-k>', vim.lsp.buf.signature_help, bufopts)
+        vim.lsp.set_log_level("debug")
+
+        cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format() ]]
+    end,
+})
+    -- local augroup_id = vim.api.nvim_create_augroup(
+    --     "FormatModificationsDocumentFormattingGroup",
+    --     { clear = false }
+    -- )
+    -- vim.api.nvim_clear_autocmds({ group = augroup_id, buffer = bufnr })
+    -- vim.api.nvim_create_autocmd(
+    --     { "BufWritePre" },
+    --     {
+    --         group = augroup_id,
+    --         buffer = bufnr,
+    --         callback = function()
+    --             local lsp_format_modifications = require"lsp-format-modifications"
+    --             lsp_format_modifications.format_modifications(client, bufnr)
+    --         end,
+    --     }
+    -- )
+
+
+-- We use the default settings for ccls and pylsp: the option table can stay empty
+-- require('lspfuzzy').setup {}  -- Make the LSP client use FZF instead of the quickfix list
 
 
 -- map('n', '<leader>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
@@ -193,14 +223,14 @@ lspfuzzy.setup {}  -- Make the LSP client use FZF instead of the quickfix list
 -- map('n', '<leader>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
 
 -------------------- DISTANT --------------------------------
-require('distant').setup {
-    -- applies chip's personal settings to every machine you connect to
-    --
-    -- 1. ensures that distant servers terminate with no connections
-    -- 2. provides navigation bindings for remote directories
-    -- 3. provides keybinding to jump into a remote file's parent directory
-    ['*'] = require('distant.settings').chip_default()
-}
+-- require('distant').setup {
+--     -- applies chip's personal settings to every machine you connect to
+--     --
+--     -- 1. ensures that distant servers terminate with no connections
+--     -- 2. provides navigation bindings for remote directories
+--     -- 3. provides keybinding to jump into a remote file's parent directory
+--     ['*'] = require('distant.settings').chip_default()
+-- }
 
 
 -------------------- Lualine --------------------------------
@@ -261,7 +291,42 @@ wk.register({
 
 
 ------------------- Neo Tree ------------------------------
--- cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+g.neo_tree_remove_legacy_commands = 1
+map('n', '<leader>nt', '<cmd>Neotree<CR>', {desc = "Neotree"})
+map('n', '<leader>nf', '<cmd>Neotree position=float<CR>', {desc = "Neotree Float"})
 -- require 'neo-tree'
 
 require('leap').add_default_mappings()
+
+------------------- CMP ------------------------------
+-- require 'cmp'.setup({
+--     snippet = {
+--       -- REQUIRED - you must specify a snippet engine
+--       expand = function(args)
+--         vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+--         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+--         -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+--         -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+--       end,
+--     },
+--     window = {
+--       -- completion = cmp.config.window.bordered(),
+--       -- documentation = cmp.config.window.bordered(),
+--     },
+--     mapping = cmp.mapping.preset.insert({
+--       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+--       ['<C-f>'] = cmp.mapping.scroll_docs(4),
+--       ['<C-Space>'] = cmp.mapping.complete(),
+--       ['<C-e>'] = cmp.mapping.abort(),
+--       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+--     }),
+--     sources = cmp.config.sources({
+--       { name = 'nvim_lsp' },
+--       -- { name = 'vsnip' }, -- For vsnip users.
+--       { name = 'luasnip' }, -- For luasnip users.
+--       -- { name = 'ultisnips' }, -- For ultisnips users.
+--       -- { name = 'snippy' }, -- For snippy users.
+--     }, {
+--         { name = 'buffer' },
+--     })
+-- })
