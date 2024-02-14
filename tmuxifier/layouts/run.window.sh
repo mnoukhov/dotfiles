b2bd4d62cbddd4bacb0fe5e200df4c6ebaf5a6e2
Michael Noukhovitch <mnoukhov@gmail.com>
2024-02-14 19:00:35 +0000

    tmuxifier layouts

---
+++ tmuxifier/layouts/run.window.sh
@@ -0,0 +1,26 @@
+# Set window root path. Default is `$session_root`.
+# Must be called before `new_window`.
+#window_root "~/Projects/{{WINDOW_NAME}}"
+
+# Create new window. If no argument is given, window name will be based on
+# layout file name.
+new_window "run"
+
+# Split window into panes.
+#split_v 20
+
+# Run commands.
+run_cmd "conda activate trl"
+run_cmd "cd examples/summarization"
+
+split_h 50
+
+run_cmd "watch -n 1 nvidia-smi"
+#run_cmd "date" 1  # runs in pane 1
+
+# Paste text
+#send_keys "top"    # paste into active pane
+#send_keys "date" 1 # paste into pane 1
+
+# Set active pane.
+select_pane 0
---
+++ tmuxifier/layouts/trl.session.sh
@@ -0,0 +1,23 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "trl"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "snow"; then
+
+  # Create a new window inline within session layout definition.
+  # new_window "In-line Window"
+  # tmux split-window -h -p 50
+
+  # Load a defined window layout.
+  load_window "run"
+  load_window "code"
+
+  # Select the default active window on session creation.
+  # select_window 0
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
