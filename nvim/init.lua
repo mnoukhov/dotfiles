4dcf47ba29094df9ec14ddeb0b1ea383ecfcfe06
Michael Noukhovitch <michaeln@allenai.org>
2025-11-17 19:27:59 -0500

    fugit, diffivew and vim.lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,6 +37,26 @@ require("lazy").setup({
         version = '0.1.x', 
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
+    {
+        'SuperBo/fugit2.nvim',
+        build = false,
+        opts = {
+            width = 100,
+        },
+        dependencies = {
+            'MunifTanjim/nui.nvim',
+            'nvim-tree/nvim-web-devicons',
+            'nvim-lua/plenary.nvim',
+            {
+                'chrisgrieser/nvim-tinygit', -- optional: for Github PR view
+                dependencies = { 'stevearc/dressing.nvim' }
+            },
+        },
+        cmd = { 'Fugit2', 'Fugit2Diff', 'Fugit2Graph' },
+        keys = {
+            { '<leader>gg', mode = 'n', '<cmd>Fugit2<cr>' }
+        }
+    },
 
     -- motions and ux
     {'numToStr/Comment.nvim'},
@@ -57,6 +77,7 @@ require("lazy").setup({
     {'tpope/vim-rhubarb'},
     {'tpope/vim-eunuch'},
     {'ggandor/leap.nvim'},
+    {'sindrets/diffview.nvim'},
 
     -- Neotree
     {"nvim-neo-tree/neo-tree.nvim", branch = "v3.x"},
@@ -158,9 +179,6 @@ map('n', '<C-h>', '<cmd>bprevious<CR>')
 map('n', '<C-l>', '<cmd>bnext<CR>')
 map('n', '<C-d>', '<cmd>bdelete<CR>')
 
--- fugitive --
-map('n', '<leader>g', '<cmd>Git ++curwin<CR>', {desc = "Git"})
-
 -- show current path --
 map('n', '<leader>p', '<cmd>echo expand("%:p")<CR>', {desc = "Show current path"})
 
@@ -227,25 +245,11 @@ vim.lsp.config('pyright', {
   }
 })
 
-vim.lsp.enable('pyright')
-
-vim.lsp.enable('autotools_ls')
-
--- local ruff_config_path = vim.loop.cwd() .. '/pyproject.toml'
-vim.lsp.config('ruff', {
-    -- init_options = {
-    --     settings = {
-    --         format = {
-    --             args = { "--config", ruff_config_path }
-    --         },
-    --         lint = {
-    --             args = { "--config", ruff_config_path }
-    --         }
-    --     }
-    -- }
+vim.lsp.enable({
+    'autotools_ls',
+    'pyright',
+    'ruff',
 })
-
-vim.lsp.enable('ruff')
 
 
 -------------------- Lualine --------------------------------
@@ -295,8 +299,8 @@ map('n', '<leader>nr', '<cmd>Neotree reveal position=float<CR>', {desc = "Neotre
 
 ------------------- Leap ------------------------------
 require('leap')
-vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
-vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
+vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)', {desc = "Leap" })
+vim.keymap.set('n',             'S', '<Plug>(leap-from-window)', {desc = "Leap from Window" })
 require('leap').opts.preview = function (ch0, ch1, ch2)
   return not (
     ch1:match('%s')
@@ -324,21 +328,10 @@ vim.keymap.set('n', '<leader>fs', builtin.grep_string, {desc="Grep String under 
 vim.keymap.set('n', '<leader>f;', builtin.jumplist, {desc="Find Jumplist"})
 vim.keymap.set('n', '<leader>f/', builtin.current_buffer_fuzzy_find, {desc="Current Buffer Fuzzy Find"})
 
--- require("telescope").setup({
---     pickers = {
---         find_files = {
---             find_command = {
---                 'fd',
---                 '--type',
---                 'f',
---                 '--no-ignore-vcs',
---                 '--color=never',
---                 '--hidden',
---                 '--follow',
---             }
---         }
---     }
--- })
+
+---- Diffview ---
+map('n', '<leader>dd', '<cmd>DiffviewOpen<CR>', {desc = "Diffview Open"})
+map('n', '<leader>dx', '<cmd>DiffviewClose<CR>', {desc = "Diffview Close"})
 
 ---- Trouble ---
 require("trouble").setup({
