5ab4974dda35d6d72c6e02786b350a634563e571
Michael Noukhovitch <michaeln@allenai.org>
2026-02-17 18:27:07 -0500

    commit from diffview

--- nvim/init.lua
+++ nvim/init.lua
@@ -65,16 +65,50 @@ require("lazy").setup({
         cmd = {
             'DiffviewFileHistory', 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles', 'DiffviewRefresh',
         },
-        keymaps = {
-		file_panel = {
-		    ['gf'] = function()
-			actions.goto_file()
-			vim.cmd 'tabclose #'
-		    end,
-	    },
-	},
-    },
+        opts = function()
+            local actions = require("diffview.actions")
+            return {
+                keymaps = {
+                    file_panel = {
+                        {
+                            "n", "cc",
+                            function()
+                                vim.ui.input({ prompt = "Commit message: " }, function(msg)
+                                    if not msg then return end
+                                    local results = vim.system({ "git", "commit", "-m", msg }, { text = true }):wait()
 
+                                    if results.code ~= 0 then
+                                        vim.notify(
+                                            "Commit failed with the message: \n"
+                                            .. vim.trim(results.stdout .. "\n" .. results.stderr),
+                                            vim.log.levels.ERROR,
+                                            { title = "Commit" }
+                                        )
+                                    else
+                                        vim.notify(results.stdout, vim.log.levels.INFO, { title = "Commit" })
+                                    end
+                                end)
+                            end,
+                            { desc = "Create commit" },
+                        },
+                        {
+                            "n", "ca",
+                            "<Cmd>Git commit --amend <bar> wincmd J<CR>",
+                            { desc = "Amend the last commit" },
+                        },
+                        {
+                            "n", "gf",
+                            function()
+                                actions.goto_file()
+                                vim.cmd('tabclose #')
+                            end,
+                            { desc = "Open file and close Diffview tab" },
+                        },
+                    },
+                },
+            }
+        end,
+    },
     -- motions and ux
     {'numToStr/Comment.nvim'},
     {
